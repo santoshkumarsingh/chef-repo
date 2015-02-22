662f56947724a0b063108844d0486c998ba42d30
Santosh <santosh.ksingh03@gmail.com>
2015-02-22 16:44:48 +0530

    Data bag

--- recipes/databag.rb
+++ recipes/databag.rb
@@ -14,3 +14,24 @@ ruby_block "data_bag" do
 	end
 	action:create
 end
+
+#  the following code sample comes from the ``seed`` recipe in the following cookbook: https://github.com/mattray/bittorrent-cookbook
+
+ruby_block "share the torrent file" do
+  block do
+    f = File.open(node['bittorrent']['torrent'],'rb')
+    #read the .torrent file and base64 encode it
+    enc = Base64.encode64(f.read)
+    data = {
+      'id'=>bittorrent_item_id(node['bittorrent']['file']),
+      'seed'=>node.ipaddress,
+      'torrent'=>enc
+    }
+    item = Chef::DataBagItem.new
+    item.data_bag('bittorrent')
+    item.raw_data = data
+    item.save
+  end
+  action :nothing
+  subscribes :create, "bittorrent_torrent[#{node['bittorrent']['torrent']}]", :immediately
+end
